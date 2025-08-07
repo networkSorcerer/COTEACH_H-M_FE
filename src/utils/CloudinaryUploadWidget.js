d4b58b57f4cd6c4cb0568cb10be457f1ca1200f4
networkSocerer <gigattfyhig@gmail.com>
2025-08-07 19:58:36 +0900

    update

--- src/utils/CloudinaryUploadWidget.js
+++ src/utils/CloudinaryUploadWidget.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Button } from "@mui/material";
+import { Button } from "react-bootstrap";
+import "../App.css";
+import "../common/style/common.style.css";
 
 const CLOUDNAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 const UPLOADPRESET = process.env.REACT_APP_CLOUDINARY_PRESET;
@@ -14,7 +16,9 @@ class CloudinaryUploadWidget extends Component {
       (error, result) => {
         if (!error && result && result.event === "success") {
           console.log("Done! Here is the image info: ", result.info);
-          // DOM 요소를 직접 조작하는 대신 props로 전달된 콜백 함수만 호출
+          document
+            .getElementById("uploadedimage")
+            .setAttribute("src", result.info.secure_url);
           this.props.uploadImage(result.info.secure_url);
         }
       } //https://cloudinary.com/documentation/react_image_and_video_upload
@@ -30,12 +34,7 @@ class CloudinaryUploadWidget extends Component {
 
   render() {
     return (
-      <Button
-        id="upload_widget"
-        size="small"
-        variant="contained"
-        color="primary"
-      >
+      <Button id="upload_widget" size="sm" className="ml-2">
         Upload Image +
       </Button>
     );
