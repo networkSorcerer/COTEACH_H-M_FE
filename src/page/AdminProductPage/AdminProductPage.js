46addef156179a3b907a2edd7f4a67c2ba7a4de6
networkSocerer <gigattfyhig@gmail.com>
2025-08-07 18:10:49 +0900

    update

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -42,6 +42,12 @@ const AdminProductPage = () => {
   }, []);
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    console.log("qqqq", query);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
