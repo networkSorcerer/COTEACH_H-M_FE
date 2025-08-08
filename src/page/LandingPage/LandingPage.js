4c30c5eaed248c2e696c1fa0e35c6cb07ed88cd2
networkSocerer <gigattfyhig@gmail.com>
2025-08-08 19:45:09 +0900

    update

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.product.productList);
-  const loading = useSelector((state) => state.product.loading);
+  const loading = useSelector((state) => state.product.loading); // loading 상태 추가
   const [query] = useSearchParams();
   const name = query.get("name");
 
@@ -21,35 +21,36 @@ const LandingPage = () => {
     );
   }, [query, dispatch]);
 
-  if (loading === true) {
-    return (
-      <div className="d-flex justify-content-center my-5">
-        <Spinner animation="border" role="status" />
-      </div>
-    );
-  }
-
-  if (!loading && productList.length === 0) {
-    return (
-      <div className="text-align-center empty-bag">
-        {name === "" ? (
-          <h2>등록된 상품이 없습니다!</h2>
-        ) : (
-          <h2>{name}과 일치한 상품이 없습니다!</h2>
-        )}
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="d-flex justify-content-center my-5">
+  //       {/* ColorRing 대신 react-bootstrap Spinner 써도 되고 */}
+  //       <Spinner animation="border" role="status" />
+  //     </div>
+  //   );
+  // }
 
   return (
     <Container>
       <Row>
-        {productList.map((item) => (
-          <Col md={3} sm={12} key={item._id}>
-            <ProductCard item={item} />
-          </Col>
-        ))}
+        {productList?.length > 0 ? (
+          productList.map((item) => (
+            <Col md={3} sm={12} key={item._id}>
+              <ProductCard item={item} />
+            </Col>
+          ))
+        ) : (
+          <div className="text-align-center empty-bag">
+            {name === "" ? (
+              <h2>등록된 상품이 없습니다!</h2>
+            ) : (
+              <h2>{name}과 일치한 상품이 없습니다!</h2>
+            )}
+          </div>
+        )}
       </Row>
     </Container>
   );
 };
+
+export default LandingPage;
