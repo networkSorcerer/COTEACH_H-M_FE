4d685a66cf527f0f509ab572a9df90a4d8059969
networkSocerer <gigattfyhig@gmail.com>
2025-08-08 19:54:49 +0900

    update

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -9,37 +9,47 @@ const LandingPage = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.product.productList);
+  const loading = useSelector((state) => state.product.loading);
   const [query] = useSearchParams();
   const name = query.get("name");
+
   useEffect(() => {
     dispatch(
       getProductList({
         name,
       })
     );
-  }, [query]);
+  }, [query, dispatch]);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center my-5">
+        <Spinner animation="border" role="status" />
+      </div>
+    );
+  }
+
+  if (!loading && productList.length === 0) {
+    return (
+      <div className="text-align-center empty-bag">
+        {name === "" ? (
+          <h2>등록된 상품이 없습니다!</h2>
+        ) : (
+          <h2>{name}과 일치한 상품이 없습니다!</h2>
+        )}
+      </div>
+    );
+  }
 
   return (
     <Container>
       <Row>
-        {productList?.length > 0 ? (
-          productList.map((item) => (
-            <Col md={3} sm={12} key={item._id}>
-              <ProductCard item={item} />
-            </Col>
-          ))
-        ) : (
-          <div className="text-align-center empty-bag">
-            {name === "" ? (
-              <h2>등록된 상품이 없습니다!</h2>
-            ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
-            )}
-          </div>
-        )}
+        {productList.map((item) => (
+          <Col md={3} sm={12} key={item._id}>
+            <ProductCard item={item} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
 };
-
-export default LandingPage;
