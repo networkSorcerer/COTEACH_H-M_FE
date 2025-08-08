5c925136a62e39c0d39c2834f08439f0ca00453d
networkSocerer <gigattfyhig@gmail.com>
2025-08-08 19:33:26 +0900

    update

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ProductCard from "./components/ProductCard";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Spinner } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
@@ -9,15 +9,25 @@ const LandingPage = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.product.productList);
+  const loading = useSelector((state) => state.product.loading); // loading 상태 추가
   const [query] = useSearchParams();
   const name = query.get("name");
+
   useEffect(() => {
     dispatch(
       getProductList({
         name,
       })
     );
-  }, [query]);
+  }, [query, dispatch]);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center my-5">
+        <Spinner animation="border" role="status" />
+      </div>
+    );
+  }
 
   return (
     <Container>
@@ -33,7 +43,7 @@ const LandingPage = () => {
             {name === "" ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{name}과 일치한 상품이 없습니다!</h2>
             )}
           </div>
         )}
