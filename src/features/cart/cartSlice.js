a18b3ab0a1715ba82f82d459d45a50f107fb3659
networkSocerer <gigattfyhig@gmail.com>
2025-08-09 20:25:47 +0900

    update

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -56,15 +56,15 @@ export const getCartList = createAsyncThunk(
 
 export const deleteCartItem = createAsyncThunk(
   "cart/deleteCartItem",
-  async (id, { rejectWithValue, dispatch }) => {
+  async (itemId, { rejectWithValue }) => {
     try {
-      console.log("delete id 잘 가져가니? ", id);
-      const response = await api.delete(`/cart/${id}`);
-      if (response.status !== 200) throw new Error(response.error);
-      dispatch(getCartList());
+      const response = await api.delete("/cart", {
+        data: { itemId }, // body로 보내야 함
+      });
+      if (response.status !== 200) throw new Error(response.data.error);
       return response.data;
     } catch (error) {
-      return rejectWithValue(error.error);
+      return rejectWithValue(error.message);
     }
   }
 );
