c5a79f57a8ad0c3e0017b4651d76b40f3773da5c
networkSocerer <gigattfyhig@gmail.com>
2025-08-17 19:17:20 +0900

    update

--- src/common/component/Navbar.js
+++ src/common/component/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = ({ user }) => {
   const { cartItemCount } = useSelector((state) => state.cart);
   const isMobile = window.navigator.userAgent.indexOf("Mobile") !== -1;
   const [showSearchBox, setShowSearchBox] = useState(false);
-  const menuList = ["여성", "남성", "신생아/유아", "아동"];
+  const menuList = ["WOMEN", "MAN", "KIDS", "HOME"];
   let [width, setWidth] = useState(0);
   let navigate = useNavigate();
   const onCheckEnter = (event) => {
